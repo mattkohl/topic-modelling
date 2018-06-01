73cbe657bc37b93245d92da862f85d9386e7151f
mattkohl <hlaford@gmail.com>
2018-06-01 06:31:37 +0100

    updates

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from gensim import corpora
 from gensim.corpora import Dictionary
+from gensim.corpora import MmCorpus
+from gensim.test.utils import datapath, get_tmpfile
 import re
 from requests import get
 from collections import defaultdict
-from typing import List, Dict
+from typing import List, Dict, Tuple, Union, Optional
 import argparse
 
 
@@ -12,7 +14,7 @@ def remove_stopwords(documents: List[str]) -> List[List[str]]:
     return [[word for word in document.lower().split() if word not in stop_list] for document in documents]
 
 
-def remove_hapax_legomena(texts: List[List[str]]):
+def remove_hapax_legomena(texts: List[List[str]]) -> List[List[str]]:
     frequency = defaultdict(int)
     for text in texts:
         for token in text:
@@ -20,12 +22,46 @@ def remove_hapax_legomena(texts: List[List[str]]):
     return [[token for token in text if frequency[token] > 1] for text in texts]
 
 
+def load_corpus() -> Optional[MmCorpus]:
+    try:
+        corpus = MmCorpus(datapath("resources/corpora/trr.mm"))
+    except Exception as e:
+        print(f"No corpus yet: {e}")
+        return None
+    else:
+        return corpus
+
+
+def build_corpus(texts: List[List[str]], dictionary: Dictionary) -> List[List[Tuple[int, int]]]:
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    return corpus
+
+
+def load_dictionary() -> Optional[Dictionary]:
+    try:
+        dictionary = Dictionary.load(f"resources/dictionaries/trr.dict")
+    except Exception as e:
+        print(f"No dictionary yet: {e}")
+        return None
+    else:
+        return dictionary
+
+
 def build_dictionary(texts: List[List[str]]) -> Dictionary:
     dictionary = corpora.Dictionary(texts)
+    dictionary.save(f"resources/dictionaries/trr.dict")
     return dictionary
 
 
-def get_sense_id(url: str) -> Dict[str, str]:
+def update_dictionary(dictionary: Optional[Dictionary], texts: List[List[str]]) -> Dictionary:
+    if dictionary:
+        dictionary.add_documents(texts)
+        return dictionary
+    else:
+        return build_dictionary(texts)
+
+
+def get_random_sense_id(url: str) -> Dict[str, str]:
     r = get(f"{url}/senses/random/")
     j = r.json()
     return {"id": j["xml_id"], "headword": j["headword"]}
@@ -37,23 +73,29 @@ def get_sense_examples(url: str, sense_id: str) -> List[str]:
     return [ex["lyric"] for ex in j["examples"]]
 
 
-def slugify(value):
+def slugify(value: str) -> str:
     value = re.sub('[^\w\s-]', '', value).strip().lower()
     value = re.sub('[-\s]+', '-', value)
     return value
 
 
-def main(url: str):
-    from pprint import pprint
-    sense = get_sense_id(url)
+def get_random_document(url) -> Dict[str, Union[str, List[List[str]]]]:
+    sense = get_random_sense_id(url)
     headword = sense["headword"]
-    print(headword)
-    fn = slugify(headword)
+    name = f"{slugify(headword)}_{sense['id']}"
     docs = get_sense_examples(url, sense["id"])
     txts = remove_stopwords(docs)
     txts_minus_hapax = remove_hapax_legomena(txts)
-    dictionary = build_dictionary(txts_minus_hapax)
-    dictionary.save(f"resources/dictionaries/{fn}.dict")
+    return {"name": name, "texts": txts_minus_hapax}
+
+
+def main(url: str) -> None:
+    from pprint import pprint
+    dictionary = load_dictionary()
+    corpus = load_corpus()
+    document = get_random_document(url)
+    pprint(document)
+    dictionary = update_dictionary(dictionary, document["texts"])
     pprint(dictionary.token2id)
 
 
